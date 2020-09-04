a23a08b38feb992fc2c2a6f15513344ac73df1ea
Prasun <pmandal@akamai.com>
2020-09-04 23:59:46 +0530

    cleanUP

--- app/src/main/java/com/prasunmondal/ananta/timetrack/Values/EnterTimeActivity.kt
+++ app/src/main/java/com/prasunmondal/ananta/timetrack/Values/EnterTimeActivity.kt
@@ -10,35 +10,28 @@ import com.prasunmondal.ananta.timetrack.R
 import com.prasunmondal.ananta.timetrack.Utils.TimeUtils
 import kotlinx.android.synthetic.main.activity_enter_time.*
 
-import java.lang.String
-import java.text.DateFormat
-import java.text.SimpleDateFormat
-import java.util.*
-
-
 class EnterTimeActivity : AppCompatActivity() {
-    var startHour: Int = 0
-    var startMin: Int = 0
-    var stopHour: Int = 0
-    var stopMin: Int = 0
-    var dateYear: Int = 0
-    var dateMonth: Int = 0
-    var dateDay: Int = 0
-    var dateToday = ""
-    var startDateTime = ""
-    var stopDateTime = ""
+
+    lateinit var startTimePicker: TimePicker
+    lateinit var stopTimePicker: TimePicker
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enter_time)
         setSupportActionBar(toolbar)
+
+        startTimePicker = findViewById(R.id.enterData_startTimePicker)
+        stopTimePicker = findViewById(R.id.enterData_stopTimePicker)
+
+        SessionData.Singleton.instance.currentCustomer.latestStartTime =
+            TimeUtils.hrMinToMs(startTimePicker.hour, startTimePicker.minute)
+        SessionData.Singleton.instance.currentCustomer.latestEndTime =
+            TimeUtils.hrMinToMs(stopTimePicker.hour, stopTimePicker.minute)
+
         addChangeListeners()
-        SessionData.Singleton.instance.currentCustomer.latestStartTime = System.currentTimeMillis()
-        SessionData.Singleton.instance.currentCustomer.latestEndTime = System.currentTimeMillis()
     }
 
     fun onClickSave(view: View) {
-//        saveData()
         goToSavePage()
     }
 
@@ -47,57 +40,16 @@ class EnterTimeActivity : AppCompatActivity() {
         startActivity(i)
     }
 
-    fun saveData() {
-
-        SessionData.Singleton.instance.currentCustomer.latestStartTime = System.currentTimeMillis()
-        val startTime = String
-            .format(
-                Locale.getDefault(),
-                "%d:%02d:%02d", startHour,
-                startMin, 0
-            )
-
-        val stopTime = String
-            .format(
-                Locale.getDefault(),
-                "%d:%02d:%02d", stopHour,
-                stopMin, 0
-            )
-
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
-        dateFormat.timeZone = TimeZone.getTimeZone("IST")
-        val format = "yyyy-MM-dd "
-        val sdf = SimpleDateFormat(format)
-
-        dateToday = sdf.format(Date())
-
-        startDateTime = dateToday + startTime + ":000"
-        stopDateTime = dateToday + stopTime + ":000"
-
-        SessionData.Singleton.instance.currentCustomer.startTime = startDateTime
-        SessionData.Singleton.instance.currentCustomer.endTime = stopDateTime
-    }
-
     private fun addChangeListeners() {
-        var startTimePicker = findViewById<TimePicker>(R.id.enterData_startTimePicker)
         startTimePicker.setOnTimeChangedListener { view, hourOfDay, minute ->
-            startHour = hourOfDay
-            startMin = minute
-            val t = TimeUtils.hrMinToMs(startTimePicker.hour, startTimePicker.minute)
-            println(t)
-            println(System.currentTimeMillis())
-            SessionData.Singleton.instance.currentCustomer.latestStartTime = t
+            SessionData.Singleton.instance.currentCustomer.latestStartTime =
+                TimeUtils.hrMinToMs(startTimePicker.hour, startTimePicker.minute)
 
         }
 
-        var stopTimePicker = findViewById<TimePicker>(R.id.enterData_stopTimePicker)
         stopTimePicker.setOnTimeChangedListener { view, hourOfDay, minute ->
-            stopHour = hourOfDay
-            stopMin = minute
-            val t = TimeUtils.hrMinToMs(stopTimePicker.hour, stopTimePicker.minute)
-            println(t)
-            println(System.currentTimeMillis())
-            SessionData.Singleton.instance.currentCustomer.latestEndTime = TimeUtils.hrMinToMs(stopTimePicker.hour, stopTimePicker.minute)
+            SessionData.Singleton.instance.currentCustomer.latestEndTime =
+                TimeUtils.hrMinToMs(stopTimePicker.hour, stopTimePicker.minute)
         }
     }
 }
